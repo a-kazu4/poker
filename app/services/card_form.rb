4ff7cd2c2a1e3105827fec90c3ed4f3b770ce2d9
a-kazu4 <kadachi@netprotections.co.jp>
2018-07-05 19:00:26 +0900

    modify error_messages

--- app/services/card_form.rb
+++ app/services/card_form.rb
@@ -17,7 +17,8 @@ class CardForm
     @hands.each_with_index do |hand, i|
       error_messages << "#{i+1}番目のカード指定文字が不正です。（#{hand}）" if hand.scan(regex).empty?
     end
-    error_messages.unshift('5つのカード指定文字を半角スペース区切りで入力してください。（例：S1 H3 D9 C13 S11）') unless error_messages.empty?
+
+    error_messages << '半角英字大文字のスート（S,H,D,C）と数字（1〜13）の組み合わせでカードを指定してください。' unless error_messages.empty?
 
     return error_messages
   end
