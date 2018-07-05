66d3c302a5a292cc3203313a73106098ee7692b4
a-kazu4 <kadachi@netprotections.co.jp>
2018-07-05 19:06:54 +0900

    add exception handling

--- app/apis/api/base.rb
+++ app/apis/api/base.rb
@@ -1,6 +1,12 @@
 module API
   class Base < Grape::API
     prefix 'api'
+    format :json
+
+    error = {error: [{msg: "不正なURLです。"}]}
+    route :any, '*path' do
+      error!(error)
+    end
 
     mount API::V1::Base
   end
--- app/apis/api/v1/base.rb
+++ app/apis/api/v1/base.rb
@@ -5,6 +5,22 @@ module API
       version 'v1'
       format :json
 
+      rescue_from ActiveRecord::RecordNotFound do |e|
+        rack_response({ message: e.message, status: 404 }.to_json, 404)
+      end
+
+      rescue_from Grape::Exceptions::ValidationErrors do |e|
+        rack_response e.to_json, 400
+      end
+
+      rescue_from :all do
+        if Rails.env.development?
+          error!({error: [{msg: "不正なリクエストです。"}]})
+        else
+          error_response(message: "Internal server error", status: 500)
+        end
+      end
+
       mount API::V1::Cards
     end
   end
