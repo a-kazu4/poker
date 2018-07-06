ca88a586ca41d2fece8aca7744b2e69442bfe744
a-kazu4 <kadachi@netprotections.co.jp>
2018-07-06 12:47:07 +0900

    add cards_spec.rb

--- spec/requests/cards_spec.rb
+++ spec/requests/cards_spec.rb
@@ -1,4 +1,24 @@
 require 'rails_helper'
 
-describe Cards, type: :request do
+describe 'API_Cards', type: :request do
+  describe 'POST /api/v1/cards/check' do
+    let(:valid_path) { '/api/v1/cards/check' }
+    let(:invalid_path) { '/api/v1' }
+    let(:valid_params) { { "cards": [ "H1 H13 H12 H11 H10", "H9 C9 S9 H2 C2", "C13 D12 C11 H8 H7" ] } }
+
+    context 'normal' do
+      it 'has a 201 status code' do
+        post valid_path, params: valid_params
+        expect(response.status).to eq 201
+      end
+    end
+
+    context 'abnormal' do
+      it 'has a 500 status code with invalid_path' do
+        post invalid_path, params: valid_params
+        expect(response.status).to eq 500
+        expect(response.body).to eq "{\"error\":[{\"msg\":\"不正なURLです。\"}]}"
+      end
+    end
+  end
 end
