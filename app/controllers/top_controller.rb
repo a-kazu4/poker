0d2d730224a821cc27f538ae82fa506f06aac0ab
a-kazu4 <kadachi@netprotections.co.jp>
2018-07-10 17:04:40 +0900

    modification after 1st review

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,5 @@
 class TopController < ApplicationController
+  include CardHelper
 
   def new
     @card_form = CardForm.new
@@ -11,10 +12,12 @@ class TopController < ApplicationController
     error_messages = @card_form.valid?
 
     if error_messages.empty?
-      flash[:hands] = @card_form.check_hands
+      flash[:hands] = CardHelper::HAND_NAME[@card_form.check_hands]
       return redirect_to top_url
     else
       error_messages.each_with_index {|message, i| flash[i] = message }
+      flash.now[:errors] = error_messages
+
       return render :new
     end
   end
--- app/services/card_form.rb
+++ app/services/card_form.rb
@@ -43,18 +43,18 @@ class CardForm
     split_cards_count = number_cards.group_by(&:itself).map{ |k, v| [k, v.count] }.to_h
 
     if suit_cards.uniq.size == 1 && serial_number_count == 5
-      return 'ストレートフラッシュ'
+      return :straight_flush
     end
     if suit_cards.uniq.size == 1 && number_cards == [1, 10, 11, 12, 13]
-      return 'ストレートフラッシュ'
+      return :straight_flush
     end
-    return 'フォー・オブ・ア・カインド' if split_cards_count.values.include?(4)
-    return 'フルハウス' if split_cards_count.values.sort & [2, 3] == [2, 3]
-    return 'フラッシュ' if suit_cards.uniq.size == 1
-    return 'ストレート' if serial_number_count == 5 || number_cards == [1, 10, 11, 12, 13]
-    return 'スリー・オブ・ア・カインド' if split_cards_count.values.include?(3)
-    return 'ツーペア' if split_cards_count.values.sort == [1, 2, 2]
-    return 'ワンペア' if split_cards_count.values.sort == [1, 1, 1, 2]
-    return 'ハイカード'
+    return :four_of_a_kind if split_cards_count.values.sort == [1, 4]
+    return :full_house if split_cards_count.values.sort == [2, 3]
+    return :flush if suit_cards.uniq.size == 1
+    return :straight if serial_number_count == 5 || number_cards == [1, 10, 11, 12, 13]
+    return :three_of_a_kind if split_cards_count.values.sort == [1, 1, 3]
+    return :two_pair if split_cards_count.values.sort == [1, 2, 2]
+    return :one_pair if split_cards_count.values.sort == [1, 1, 1, 2]
+    return :high_card
   end
 end
